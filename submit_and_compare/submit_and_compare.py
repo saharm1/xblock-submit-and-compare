7129280052b63c3e26fa62584bbaab8572e2a927
= <azim.pradhan@gmail.com>
2015-12-03 13:44:56 -0800

    Add grading to Submit and Compare XBlock
    This commit adds grading to the Submit
    and Compare Xblock. Instructors can specify
    the weight of the XBlock and as long as
    a student responds with a non-blank response,
    full-credit will be awarded.

--- submit_and_compare/submit_and_compare.py
+++ submit_and_compare/submit_and_compare.py
@@ -1,18 +1,16 @@
 '''  Submit and Compare XBlock main Python class'''
 
-import pkg_resources
-from django.template import Context, Template
-
-from xblock.core import XBlock
-from xblock.fields import Scope, String, List
-from xblock.fragment import Fragment
-
-from lxml import etree
-from xml.etree import ElementTree as ET
-
+import textwrap
 from StringIO import StringIO
 
-import textwrap
+import pkg_resources
+from django.template import Context, Template
+from django.utils.translation import ungettext
+from lxml import etree
+from xblock.core import XBlock
+from xblock.fields import Scope, String, List, Float, Integer
+from xblock.fragment import Fragment
+
 
 class SubmitAndCompareXBlock(XBlock):
 
@@ -72,6 +70,20 @@ class SubmitAndCompareXBlock(XBlock):
         '''
         ))
 
+    score = Float(
+        default=0.0,
+        scope=Scope.user_state,
+    )
+
+    weight = Integer(
+        display_name='Weight',
+        help='This assigns an integer value representing '
+             'the weight of this problem',
+        default=1,
+        scope=Scope.settings,
+    )
+
+    has_score = True
 
     '''
     Main functions
@@ -81,13 +93,15 @@ class SubmitAndCompareXBlock(XBlock):
         The primary view of the XBlock, shown to students
         when viewing courses.
         '''
+        problem_progress = self._get_problem_progress()
         prompt = self._get_body(self.question_string)
         explanation = self._get_explanation(self.question_string)
-        
+
         attributes = ''
         html = self.resource_string('static/html/submit_and_compare_view.html')
         frag = Fragment(html.format(display_name = self.display_name,
-        							prompt = prompt, 
+                                    problem_progress=problem_progress,
+                                    prompt = prompt,
                                     student_answer = self.student_answer, 
                                     explanation = explanation, 
                                     your_answer_label = self.your_answer_label,
@@ -106,6 +120,7 @@ class SubmitAndCompareXBlock(XBlock):
         '''
         context = {
             'display_name': self.display_name,
+            'weight': self.weight,
             'xml_data': self.question_string,
             'your_answer_label': self.your_answer_label,
             'our_answer_label': self.our_answer_label,
@@ -123,7 +138,18 @@ class SubmitAndCompareXBlock(XBlock):
         Save student answer
         '''
         self.student_answer = submissions['answer']
-        return {'success':True}
+
+        if self.student_answer:
+            self.score = self.weight
+        else:
+            self.score = 0.0
+
+        self._publish_grade()
+        result = {
+            'success':True,
+            'problem_progress':self._get_problem_progress(),
+        }
+        return result
 
     @XBlock.json_handler
     def studio_submit(self, submissions, suffix=''):
@@ -131,6 +157,12 @@ class SubmitAndCompareXBlock(XBlock):
         Save studio edits
         '''
         self.display_name = submissions['display_name']
+        try:
+            weight = int(submissions['weight'])
+        except ValueError:
+            weight = 0
+        if weight > 0:
+            self.weight = weight
         self.your_answer_label = submissions['your_answer_label']
         self.our_answer_label = submissions['our_answer_label']
         xml_content = submissions['data']
@@ -151,25 +183,25 @@ class SubmitAndCompareXBlock(XBlock):
     @XBlock.json_handler
     def send_hints(self, submissions, suffix=''):
         
-		tree = etree.parse(StringIO(self.question_string))
-		raw_hints = tree.xpath('/submit_and_compare/demandhint/hint')
-		
-		decorated_hints = list()
-		
-		if len(raw_hints) == 1:
-			hint = 'Hint: ' + etree.tostring(raw_hints[0], encoding='unicode')
-			decorated_hints.append(hint)
-		else:
-			for i in range(len(raw_hints)):
-				hint = 'Hint (' + str(i+1) + ' of ' + str(len(raw_hints)) + '): ' + etree.tostring(raw_hints[i], encoding='unicode')
-				decorated_hints.append(hint)
-		
-		hints = decorated_hints	
+        tree = etree.parse(StringIO(self.question_string))
+        raw_hints = tree.xpath('/submit_and_compare/demandhint/hint')
 
-		return {
-			'result': 'success',
-			'hints': hints,
-		}
+        decorated_hints = list()
+
+        if len(raw_hints) == 1:
+            hint = 'Hint: ' + etree.tostring(raw_hints[0], encoding='unicode')
+            decorated_hints.append(hint)
+        else:
+            for i in range(len(raw_hints)):
+                hint = 'Hint (' + str(i+1) + ' of ' + str(len(raw_hints)) + '): ' + etree.tostring(raw_hints[i], encoding='unicode')
+                decorated_hints.append(hint)
+
+        hints = decorated_hints
+
+        return {
+            'result': 'success',
+            'hints': hints,
+        }
 
     @XBlock.json_handler
     def publish_event(self, data, suffix=''):
@@ -231,3 +263,38 @@ class SubmitAndCompareXBlock(XBlock):
             # workaround for xblock workbench
             unique_id = 'workbench-workaround-id'
         return unique_id
+
+    def _get_problem_progress(self):
+        """
+        Returns a statement of progress for the XBlock, which depends
+        on the user's current score
+        """
+        result = ''
+        if self.score == 0.0:
+            result = ungettext(
+                '{weight} point possible',
+                '{weight} points possible',
+                self.weight,
+            ).format(
+                weight=self.weight
+            )
+        else:
+            score_string = '{0:g}'.format(self.score)
+            result = ungettext(
+                score_string + '/' + "{weight} point",
+                score_string + '/' + "{weight} points",
+                self.weight,
+            ).format(
+                weight=self.weight
+            )
+        return result
+
+    def _publish_grade(self):
+        self.runtime.publish(
+            self,
+            'grade',
+            {
+                'value': self.score,
+                'max_value': self.weight,
+            }
+        )
