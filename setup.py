b5f219a7bd29497a2d1fab43eee1cae4ddb83156
caseylitton <caseylitton@gmail.com>
2017-02-10 14:11:04 -0800

    Update xblock version for release

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class Tox(TestCommand):
 
 setup(
     name='xblock-submit-and-compare',
-    version='0.5',
+    version='0.5.1',
     description='Submit and Compare XBlock for self assessment',
     packages=[
         'submit_and_compare',
